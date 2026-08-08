0d66730fadd49187fbb8d49946e85c9761274397
Vineet Pandey <68551851+TheVineet@users.noreply.github.com>
2026-08-08 16:29:58 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = []
+        for s in strs:
+            res.append(f"{len(s)}#{s}")
+        return "".join(res)
+
+    def decode(self, s: str) -> List[str]:
+        
+        res = []
+        i = 0
+
+        while i < len(s):
+            numS = ""
+            # Get the first number
+            while s[i] != "#":
+                numS = numS + s[i]
+                i +=1
+                
+            # convert it into int
+            numI = int(numS)
+            
+            # loop over #
+            i +=1
+            word = s[i:numI+i]
+            res.append(word)
+            i += numI
+        
+        return res
+  
+
+
+            
+            
+
+        
